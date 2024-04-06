1aded164d20b290d1b143fd4dec4cb1aca24df21
a5bbbbb <suprunoviktor@gmail.com>
2024-04-06 19:44:11 +0500

    Commit 7: Solution for task 4 was implemented.

---
+++ src/solutions/SolutionTask4.java
@@ -0,0 +1,36 @@
+package solutions;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class SolutionTask4 implements SolutionInterface{
+    @Override
+    public String getParametersDescription() {
+        return "Enter a single number n.";
+    }
+
+    @Override
+    public int getTaskNumber() {
+        return 4;
+    }
+
+    @Override
+    public int getNumberOfInputLines() {
+        return 1;
+    }
+
+    @Override
+    public String execute(String arguments) {
+        Scanner scan = new Scanner(arguments);
+        int n;
+        try{
+            n = scan.nextInt();
+        }catch (Exception ignored){
+            return "Input is incorrect";
+        }
+        BigInteger factorial = BigInteger.ONE;
+        for(int i = 2; i <= n; i++)
+            factorial = factorial.multiply(BigInteger.valueOf(i));
+        return String.valueOf(factorial);
+    }
+}
