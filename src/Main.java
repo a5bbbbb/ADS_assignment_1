a13fbcfcd206f06c6d1bb08b7f57c0000251ff6c
a5bbbbb <suprunoviktor@gmail.com>
2024-04-07 22:23:05 +0500

    Commit 9: Some documentation added.

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,15 @@ public class Main {
                 """);
     }
 
+    /*
+    * getSolution
+    *
+    * Just a linear search through the list of solutions.
+    *
+    * @param taskNumber number of the task required to be found.
+    *
+    * @return SolutionInterface a solution with corresponding task number.
+     */
     private static SolutionInterface getSolution(int taskNumber) throws Exception {
 
         for (SolutionInterface solution : solutions)
@@ -35,36 +44,74 @@ public class Main {
         throw new Exception("No task with number " + taskNumber + " found.");
     }
 
+    /*
+    * readArguments
+    *
+    * Reads certain number of line from console, and returns them as a single String object.
+    *
+    * @param numberOfLines numbers of line that needs to be read from the console.
+    *
+    * @return String object containing concatenation of read lines.
+     */
     private static String readArguments(int numberOfLines){
+
         System.out.println("Enter input (" + numberOfLines + " lines):");
+
         StringBuilder total = new StringBuilder();
+
         for(int i = 0; i < numberOfLines + 1; i++){
-            total.append(' ');
-            total.append(scan.nextLine());
+
+            total.append(' ').append(scan.nextLine());
+
         }
+
         return total.toString();
     }
 
-    private static void executeSolution(){
+    private static int askTaskNumber(){
+
         System.out.print("""
                 Please enter number of a task that you wish to test:""");
 
-        int taskNumber = scan.nextInt();
+        return scan.nextInt();
+    }
+
+    /*
+    * executeSolution
+    *
+    * Executes the solution with corresponding task number.
+    *
+    * @param taskNumber number of the task for which solution is expected to be executed.
+     */
+    private static void executeSolution(int taskNumber){
+
         SolutionInterface solution;
+
         try {
+
             solution = getSolution(taskNumber);
+
         } catch (Exception e) {
+
             System.out.println(e.getMessage());
+
             return;
+
         }
+
         System.out.println(solution.getParametersDescription());
+
         String response = solution.execute(readArguments(solution.getNumberOfInputLines()));
+
         System.out.println("Output:");
+
         System.out.println(response);
     }
 
     public static void main(String[] args) {
+
         printHello();
-        executeSolution();
+
+        executeSolution(askTaskNumber());
     }
 }
